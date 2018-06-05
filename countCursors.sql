33e88c62a14c1796a53c96548219177048cae705
igorvood <balitsky_igor@list.ru>
2018-06-05 21:31:06 +0300

    qwewqe

---
+++ countCursors.sql
@@ -0,0 +1,7 @@
+select a.value, s.program, s.username, s.sid, s.serial#
+from v$sesstat a, v$statname b, v$session s
+where a.statistic# = b.statistic#  and s.sid=a.sid
+and b.name = 'opened cursors current'
+/*and s.username = 'VOOD'
+and s.program like '%JDBC%'*/
+order by 1 desc
